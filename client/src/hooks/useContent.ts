a965673062ef147d4beb865d0bc191ac3168d074
Virendra Patil <virendrapatil.24vp@gmail.com>
2025-01-26 23:22:45 +0530

    adds usecontent hook

---
+++ client/src/hooks/useContent.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useEffect, useState, useCallback } from "react";
+
+
+export const useContent = (intervalDuration = 10 * 1000) => {
+    const [content, setContent] = useState([]);
+    const apiURL = import.meta.env.VITE_API_URL;
+
+    const refresh = useCallback(async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const response = await axios.get(
+                `${apiURL}/api/v1/content/`,
+                {
+                    headers: {
+                        token: token || "",
+                    },
+                }
+            );
+            setContent(response.data.content);
+        } catch (error) {
+            console.error("Error fetching content:", error);
+        }
+    }, [apiURL]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            refresh();
+        }, intervalDuration);
+
+        refresh();
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [refresh, intervalDuration]);
+
+    return { refresh, content };
+};
